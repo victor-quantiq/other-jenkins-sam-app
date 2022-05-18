602a17f0defcd986ff72737698dde2180e3f8ce4
vgrau98 <victor.grau93@gmail.com>
2022-05-18 17:43:31 +0200

    ttes=

--- hello_world/app.py
+++ hello_world/app.py
@@ -37,7 +37,6 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
-            "test": "tests"
             # "location": ip.text.replace("\n", "")
         }),
     }
